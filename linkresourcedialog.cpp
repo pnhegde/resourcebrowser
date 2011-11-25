3a3e8f0c643c73c9943d209033066557b1faaf67
Phaneendra Hegde <phaneendra.hegde@gmail.com>
2011-11-25 05:39:34 +0530

    Listview updated

--- linkresourcedialog.cpp
+++ linkresourcedialog.cpp
@@ -2,24 +2,31 @@
 
 #include <QLabel>
 #include <QVBoxLayout>
+#include <QListView>
 
 #include <KLocale>
 #include <KIcon>
+#include <KEditListBox>
 #include <Nepomuk/File>
+#include <QPushButton>
 
 LinkResourceDialog::LinkResourceDialog(Nepomuk::Resource resource,QWidget* parent):KDialog(parent)
 {
     setWindowTitle(i18n("Resource Link Dialog"));
     setWindowIcon(KIcon("nepomuk"));
-    setFixedSize(300,200);
+    setButtonText(Ok,i18n("&Done"));
 
-    QVBoxLayout* vbLayout = new QVBoxLayout(this);
-    QLabel *resourceName = new QLabel(this);
+    QVBoxLayout* vbLayout = new QVBoxLayout(mainWidget());
+    QLabel *resourceName = new QLabel(mainWidget());
     resourceName->setText(resource.genericLabel()+" : "+resource.className());
-    m_resourceSearch = new KLineEdit(this);
+    m_resourceSearch = new KLineEdit(mainWidget());
     m_resourceSearch->setPlaceholderText(i18n("Search for resources"));
     vbLayout->addWidget(resourceName);
     vbLayout->addWidget(m_resourceSearch);
+    QListView* resourceLinkView = new QListView(mainWidget());
+    vbLayout->addWidget(resourceLinkView);
+    QPushButton* unlinkButton = new QPushButton(resourceLinkView->viewport());
+    unlinkButton->setIcon(KIcon("edit-delete"));
 
 
 }
